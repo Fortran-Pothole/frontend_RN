834760d4daeccabcd1905148516029be7f540c0b
jangjieun0675 <jangji0513@naver.com>
2024-08-31 00:11:17 +0900

    [UI] : 포트홀 정보창 위치 수정

--- src/components/PotholeInfo.js
+++ src/components/PotholeInfo.js
@@ -1,21 +1,20 @@
 import React from 'react';
-import { View, Text, StyleSheet, Image } from 'react-native';
+import {View, Text, StyleSheet, Image} from 'react-native';
 import checkDistance from '../../types/checkDistance';
 
-
-function PotholeInfo({ position, myPosition}) {
+function PotholeInfo({position, myPosition}) {
   const encodedURL = position.image ? encodeURI(position.image) : null;
   const warningLevel = position.warning;
 
   let warningText = '';
-  let warningColor = '#000000'; 
+  let warningColor = '#000000';
 
   if (warningLevel >= 1 && warningLevel <= 3) {
     warningText = '주의';
-    warningColor = '#FFA500'; 
+    warningColor = '#FFA500';
   } else if (warningLevel >= 4 && warningLevel <= 5) {
     warningText = '위험';
-    warningColor = '#FF0000'; 
+    warningColor = '#FF0000';
   }
 
   return (
@@ -24,7 +23,7 @@ function PotholeInfo({ position, myPosition}) {
       <Text style={styles.potholeInfoText}>
         전방 {Math.round(checkDistance(myPosition, position))}m
       </Text>
-      <Text style={[styles.potholeInfoText, { color: warningColor }]}>
+      <Text style={[styles.potholeInfoText, {color: warningColor}]}>
         포트홀 위험도: {warningText}
       </Text>
       <Text style={styles.potholeInfoText}>
@@ -35,10 +34,14 @@ function PotholeInfo({ position, myPosition}) {
       </Text>
       <View style={styles.imageContainer}>
         <Image
-          source={encodedURL ? { uri: encodedURL } : require('../assets/pothole_example.png')}
+          source={
+            encodedURL
+              ? {uri: encodedURL}
+              : require('../assets/pothole_example.png')
+          }
           style={styles.potholeImage}
-          resizeMode="cover" 
-          onError={() => console.log('Failed to load image')} 
+          resizeMode="cover"
+          onError={() => console.log('Failed to load image')}
         />
       </View>
     </View>
@@ -49,7 +52,7 @@ const styles = StyleSheet.create({
   potholeInfoContainer: {
     position: 'absolute',
     top: 20,
-    right: 20,
+    right: 3,
     backgroundColor: 'white',
     padding: 10,
     borderRadius: 10,
@@ -59,10 +62,21 @@ const styles = StyleSheet.create({
     height: 320,
     borderColor: '#ccc',
   },
-  potholeInfoHeader: { color: 'black', fontSize: 18, fontWeight: 'bold', marginBottom: 15 },
-  potholeInfoText: { color: 'black', fontSize: 16, marginBottom: 5 },
-  imageContainer: { marginTop: 15, width: '100%', height: 100, alignItems: 'center', justifyContent: 'center' },
-  potholeImage: { width: '100%', height: '100%', resizeMode: 'contain' },
+  potholeInfoHeader: {
+    color: 'black',
+    fontSize: 18,
+    fontWeight: 'bold',
+    marginBottom: 15,
+  },
+  potholeInfoText: {color: 'black', fontSize: 16, marginBottom: 5},
+  imageContainer: {
+    marginTop: 15,
+    width: '100%',
+    height: 100,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  potholeImage: {width: '100%', height: '100%', resizeMode: 'contain'},
 });
 
 export default PotholeInfo;
